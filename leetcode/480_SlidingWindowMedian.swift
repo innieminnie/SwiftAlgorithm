b2943a9a0e96aded7cee6b9f2466f7c134f40c0b
innieminnie <rkddlsl1001@gmail.com>
2020-08-23 15:25:55 +0900

    refactor: refactor code for 480_SlidingWindowMedian [Binary Search] [Sliding Window]

--- leetcode/480_SlidingWindowMedian.swift
+++ leetcode/480_SlidingWindowMedian.swift
@@ -5,70 +5,68 @@
 //  Created by 강인희 on 2020/07/23.
 //  Copyright © 2020 innie. All rights reserved.
 //
+/*
+ Submission Detail:
+    Runtime: 188 ms, faster than 100.00% of Swift online submissions for Sliding Window Median.
+    Memory Usage: 21.6 MB, less than 90.91% of Swift online submissions for Sliding Window Median.
+ 
+ Time Complexity : O(nlogn) for binarysearching elements in given array
+ Space Complexity : O(k) for sliding window
+ 
+ 헤멘 부분 : insertion과 removal 할 위치찾기를 같은 방식의 binary search를 이용한다는 점. 최대 최소가 아닌 중간값을 찾아야한다.
+ */
 
 import Foundation
 import XCTest
 
-func getMedian(_ arr: inout [Int])->Double{
-    if arr.count % 2 != 0{
-        return Double(arr[arr.count/2])
-    }else{
-        return Double((Double(arr[arr.count/2])+Double(arr[arr.count/2-1]))/2.0)
-    }
-}
-
-func binaryRemove(_ num: Int, arr: inout [Int]){
-    var left=0
-    var right=arr.count-1
-    while left<=right{
-        let mid=left+(right-left)/2
-        if(num>arr[mid]){
-            left=mid+1
-        }else if num<arr[mid]{
-            right=mid-1
-        }else{
-            left=mid
-            break
-        }
-    }
-    arr.remove(at:left)
-}
-
-func binaryInsert(_ num: Int, arr: inout [Int]){
-    var left=0
-    var right=arr.count-1
-    while left<=right{
-        let mid=left+(right-left)/2
-        if num>arr[mid]{
-            left=mid+1
-        }else if num<arr[mid]{
-            right=mid-1
-        }else{
-            left=mid
-            break
-        }
-    }
-    arr.insert(num, at:left)
-}
-
-func medianSlidingWindow(_ nums: [Int], _ k: Int) -> [Double] {
-    guard nums.count > 0 else{return [] }
-    var medians=[Double]()
-    var slidingWindow=[Int]()
+class SlidingWindowMedian{
     
-    for i in 0..<nums.count{
-        if i<k{
-            binaryInsert(nums[i], arr: &slidingWindow)
+    func getMedian(_ arr: inout [Int])->Double{
+        if arr.count % 2 != 0{
+            return Double(arr[arr.count/2])
         }else{
-            medians.append(getMedian(&slidingWindow))
-            binaryRemove(nums[i-k],arr: &slidingWindow)
-            binaryInsert(nums[i],arr: &slidingWindow)
+            return Double((Double(arr[arr.count/2])+Double(arr[arr.count/2-1]))/2.0)
         }
     }
-    medians.append(getMedian(&slidingWindow))
-    return medians
+    func binarySearch(_ num:Int, _ arr:inout[Int])->Int{
+        var left=0
+        var right=arr.count-1
+        
+        while left<=right{
+            let mid=left + (right - left) / 2
+            if num > arr[mid]{
+                left = mid + 1
+            }else if num < arr[mid]{
+                right=mid-1
+            }else{
+                left=mid
+                break
+            }
+        }
+        return left
+    }
+    
+    func medianSlidingWindow(_ nums: [Int], _ k: Int) -> [Double] {
+        guard nums.count > 0 else{return [] }
+        var medians=[Double]()
+        var slidingWindow=[Int]()
+        
+        for i in 0..<nums.count{
+            if i<k{
+                let idx = binarySearch(nums[i], &slidingWindow)
+                slidingWindow.insert(nums[i], at: idx)
+            }else{
+                medians.append(getMedian(&slidingWindow))
+                let removeIdx = binarySearch(nums[i-k], &slidingWindow)
+                slidingWindow.remove(at: removeIdx)
+                let insertIdx = binarySearch(nums[i], &slidingWindow)
+                slidingWindow.insert(nums[i], at: insertIdx)
+            }
+        }
+        medians.append(getMedian(&slidingWindow))
+        return medians
+    }
 }
-
 extension SolutionTests{
     func testmedainSlidingWindow_case1() {
         // Given-When-Then (준비-실행-검증)
@@ -80,7 +78,7 @@ extension SolutionTests{
         // When
         let expected:[Double]
         expected = [1,-1,-1,3,5,6]
-        let actual = medianSlidingWindow(nums,k)
+        let actual = SlidingWindowMedian().medianSlidingWindow(nums,k)
         
         // Then
         XCTAssertEqual(expected, actual)
